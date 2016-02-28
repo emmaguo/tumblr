bc8e7880b4aae972bd920297511b7c97804e4da3
Emma Guo <emmahongguo@gmail.com>
2016-02-27 20:16:19 -0800

    Implement TabBarViewController behavior

--- tumblr/TabBarViewController.swift
+++ tumblr/TabBarViewController.swift
@@ -11,13 +11,53 @@ import UIKit
 class TabBarViewController: UIViewController {
 
     @IBOutlet weak var contentView: UIView!
+    @IBOutlet var buttons: [UIButton]!
+    
+    var homeViewController: UIViewController!
+    var searchViewController: UIViewController!
+    var composeViewController: UIViewController!
+    var accountViewController: UIViewController!
+    var trendingViewController: UIViewController!
+    var viewControllers: [UIViewController]!
+    var selectedIndex: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        homeViewController = storyboard.instantiateViewControllerWithIdentifier("HomeViewStoryboard")
+        searchViewController = storyboard.instantiateViewControllerWithIdentifier("SearchViewStoryboard")
+        composeViewController = storyboard.instantiateViewControllerWithIdentifier("ComposeViewStoryboard")
+        accountViewController = storyboard.instantiateViewControllerWithIdentifier("AccountViewStoryboard")
+        trendingViewController = storyboard.instantiateViewControllerWithIdentifier("TrendingViewStoryboard")
+        viewControllers = [
+            homeViewController,
+            searchViewController,
+            composeViewController,
+            accountViewController,
+            trendingViewController
+        ]
+        onPressTab(buttons[selectedIndex])
     }
 
+    @IBAction func onPressTab(sender: UIButton) {
+        let previousIndex = selectedIndex
+        buttons[previousIndex].selected = false
+        let previousVC = viewControllers[previousIndex]
+        previousVC.willMoveToParentViewController(nil)
+        previousVC.view.removeFromSuperview()
+        previousVC.removeFromParentViewController()
+        
+        selectedIndex = sender.tag
+        sender.selected = true
+        let vc = viewControllers[selectedIndex]
+        addChildViewController(vc)
+        vc.view.frame = contentView.bounds
+        contentView.addSubview(vc.view)
+        vc.didMoveToParentViewController(self)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
